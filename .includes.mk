dc1f5bda0919e05bb5a88d77d04e0623afc5f044
genchen <chengen@huawei.com>
2026-07-02 20:39:21 +0800

    optimize complying to Per's feedback with some reversion

--- .includes.mk
+++ .includes.mk
@@ -1,3 +1,3 @@
-draft-bcmj-green-power-and-energy-yang.xml: yang/ietf-power-and-energy.txt
-draft-bcmj-green-power-and-energy-yang.xml: yang/ietf-power-and-energy.yang
-draft-bcmj-green-power-and-energy-yang.xml: yang/iana-ietf-power-and-energy.yang
+draft-ietf-green-power-and-energy-yang.xml: yang/ietf-power-and-energy.txt
+draft-ietf-green-power-and-energy-yang.xml: yang/ietf-power-and-energy.yang
+draft-ietf-green-power-and-energy-yang.xml: yang/iana-power-and-energy.yang
